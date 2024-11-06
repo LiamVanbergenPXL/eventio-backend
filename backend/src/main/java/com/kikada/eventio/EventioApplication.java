2f2f756f044226497eb6e6d61ba305792f2165d6
LiamVanbergenPXL <liam.vanbergen@student.pxl.be>
2024-11-06 22:19:25 +0100

    Backend springboot

---
+++ backend/src/main/java/com/kikada/eventio/EventioApplication.java
@@ -0,0 +1,13 @@
+package com.kikada.eventio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventioApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/kikada/eventio/EventioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kikada.eventio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
