01ce5f29df1b11321e52eb7da2b7c92c61061bf7
LiamVanbergenPXL <liam.vanbergen@student.pxl.be>
2024-11-11 20:14:34 +0100

    complition RegisterForm.jsx

--- frontend/src/Components/RegisterForm/RegisterForm.jsx
+++ frontend/src/Components/RegisterForm/RegisterForm.jsx
@@ -4,12 +4,15 @@ import user_icon from  '../Assets/person.png';
 import email_icon from '../Assets/email.png';
 import pass_icon from '../Assets/password.png';
 import axios from "axios";
-import {Link} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
+
 
 const  RegisterForm = () => {
     const [username, setUsername] = useState("");
     const  [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [showAlert, setShowAlert] = useState(false);
+    const navigate = useNavigate();
 
     async function save(event){
         event.preventDefault();
@@ -19,13 +22,30 @@ const  RegisterForm = () => {
                 email: email,
                 password: password
             });
-            alert("Succesfull login");
+            setShowAlert(true);
+            setTimeout(() => {
+                navigate("/login");}
+                , 3000
+            )
         } catch (err){
             alert(err);
         }
     }
     return(
         <div className="container flex flex-col m-auto bg-white">
+            {showAlert && (
+                <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative"
+                     role="alert">
+                    <strong className="font-bold">Succes! </strong>
+                    <span className="block sm:inline">Je account is succesvol aangemaakt!</span>
+                    <span className="absolute top-0 bottom-0 right-0 px-4 py-3" onClick={() => setShowAlert(false)}>
+                        <svg className="fill-current h-6 w-6 text-red-500" role="button"
+                             xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path
+                            d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+                    </span>
+                    <div className="completion-bar bg-green-400"></div>
+                </div>
+            )}
             <div className="header flex flex-col items-center w-full">
                 <div className="text">
                     Aanmelden
